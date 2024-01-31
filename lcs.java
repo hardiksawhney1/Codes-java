a4b58ad6237b2fe85eb7a1dab22ca59ae2f3e4fc
hardiksawhney1 <123488390+hardiksawhney1@users.noreply.github.com>
2024-02-01 02:13:53 +0530

    Update lcs.java

--- lcs.java
+++ lcs.java
@@ -1,25 +1,22 @@
-public class lcs {
-    public static void main(String[] args) {
-        String s1 = "aabba";
-        String s2 ="aaaaa";
-        int i=0;
-        int j=0;
-        int count=0;
-        while(i<s1.length() && j<s2.length()){
-            if(s1.charAt(i)==s2.charAt(j)){
-                i++; 
-                j++;
-                count++;
-            }
-            else{
-                if(s1.length()>s2.length()){
-                    i++;
-                }
-                else{
-                    j++;
-                }
-            }
+import java.util.Scanner;
+
+public class longestConsecutiveSubsequence {
+
+    public  static int lcs(String x, String y, int m, int n){
+        if (m==0||n==0) return 0;
+
+        if (x.charAt(m-1)==y.charAt(n-1)){
+            return 1+lcs(x,y,m-1,n-1);
         }
-        System.out.println(count);
+        else{
+            return Math.max(lcs(x,y,m-1,n), lcs(x,y,m,n-1));
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String x = sc.nextLine();
+        String y = sc.nextLine();
+
+        System.out.println(lcs(x,y,x.length(),y.length()));
     }
 }
